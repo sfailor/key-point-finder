19c4d5b7d610ee44d247c5203bd8ef2e650b00f2
Samuel W. Failor <samuel.failor@gmail.com>
2023-04-14 22:00:18 +0100

    packaging

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='key-point-finder',
+    version='0.0.1',
+    author = 'Samuel W. Failor',
+    author_email = 'samuel.failor@gmail.com',
+    url = 'http://github.com/sfailor/key-point-finder',
+    install_requires=   [
+                            'pyqtgraph',
+                            'PyQt5',
+                            'numpy',                       
+                        ],
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
