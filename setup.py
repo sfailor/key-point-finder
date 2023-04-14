6b82ad0f8de1eed81a5b80215cda6fca410f29d0
Samuel W. Failor <samuel.failor@gmail.com>
2023-04-14 22:40:58 +0100

    simplifies import

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='key-point-finder',
     version='0.0.1',
-    author = 'Samuel W. Failor',
-    author_email = 'samuel.failor@gmail.com',
-    url = 'http://github.com/sfailor/key-point-finder',
+    author='Samuel W. Failor',
+    author_email='samuel.failor@gmail.com',
+    url='http://github.com/sfailor/key-point-finder',
     install_requires=   [
                             'pyqtgraph',
                             'PyQt5',
